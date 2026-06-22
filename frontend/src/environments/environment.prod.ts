b6eeddc8cab1e33fa4a2bc2f68c686e8be5da368
Soraia G. Siquelli <114179140+soraiasiquelli@users.noreply.github.com>
2026-06-21 21:46:49 -0300

    Correção URL Back

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://sorai7099.c44.integrator.host:3000'
+  apiUrl: 'http://sorai7099.c44.integrator.host:62635/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://sorai7099.c44.integrator.host:3000'
+  apiUrl: 'http://sorai7099.c44.integrator.host:62635/'
 };
